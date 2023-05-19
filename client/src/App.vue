814f38d6f924130f4ea5094e7687740988ad74c2
AB498 <abcd49800@gmail.com>
2023-05-19 09:59:51 +0000

    dasaf

--- client/src/App.vue
+++ client/src/App.vue
@@ -127,7 +127,7 @@ if (window.glb.user) {
                   </span>
                 </span>
                 <div class="font-Galada text-4xl p-1">
-                      Lx
+                          Lx
                 </div>
               </RouterLink>
             </template>
