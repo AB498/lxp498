d124fd0c3aff6026cfd4bf27cfb113ded0a989a6
AB498 <abcd49800@gmail.com>
2023-05-20 19:42:34 +0000

    gi7:42 PM201406 PM9027:42 PM140 p902347

--- server/controllers/progressController.js
+++ server/controllers/progressController.js
@@ -74,7 +74,7 @@ module.exports.updateProgress = async (req, res) => {
     return res.send(progress);
 }
 
-module.exports.getProgress = async (req, res, next) => {
+module.exports.getProgress = async (req, res) => {
 
     const user = req.user;
     let progresses = await models.Progress.findAll({ where: { UserId: user.id, language: language } });
