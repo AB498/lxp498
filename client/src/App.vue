65f2d4d339d562a28949958779d47f238de0d29d
AB498 <abcd49800@gmail.com>
2023-05-20 11:05:36 +0000

    gi11:05 AM201406 AM54911:05 AM140 p5493611

--- client/src/App.vue
+++ client/src/App.vue
@@ -158,7 +158,7 @@ const searchText = ref("");
 
             <div
                 class="  rounded-full h-8 self-center mx-2 bg-zinc-700 flex focus-within:bg-gray-600 focus-within:ring-2 ring-blue-500 transition-all">
-                        <input class="p-2 w-52 h-full rounded-full without-ring bg-transparent transition-all " placeholder="Search ..."
+                          <input class="px-4 w-52 h-full rounded-full without-ring bg-transparent transition-all " placeholder="Search ..."
                         :class="searchCollapsed ? 'w-0 p-0 m-0' : ''" v-model="searchText"/>
                 <a href=""
                     class="fa hover:scale-125 text-gray-400 self-center hover:text-blue-500 w-8 h-8 rounded-full flex center" 
