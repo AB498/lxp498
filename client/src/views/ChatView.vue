164b9cd025abbee6fb63a6654eed4547f0d7cb58
AB498 <abcd49800@gmail.com>
2023-05-20 21:04:50 +0000

    gi9:04 PM201406 PM1659:04 PM140 p165509

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -38,6 +38,14 @@ watch(() => window.glb.lxsocket.onlineUsers, (newVal, oldVal) => {
             </div>
           </div>
         </div>
+          <div class="text-2xl p-2" v-loading-bar="{ loading: !window.glb._serverSynced.onlineUsers }" v-ripple>Online</div>
+          <div class="" v-if="window.glb._serverSynced.offlineUsers">
+            <div v-for="(user, index) in window.glb._serverSynced.onlineUsers" :key="index">
+              <div class="btn" @click="" v-ripple>
+                {{ user.email }}
+              </div>
+            </div>
+          </div>
       </div>
       <div class="basis-3/4 h-full 0 overflow-auto ">
         <RouterView></RouterView>
