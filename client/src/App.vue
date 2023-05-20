29b1342f32beb77e84a1fc9b6b4eb69b8793ccd4
AB498 <abcd49800@gmail.com>
2023-05-20 11:08:34 +0000

    gi11:08 AM201406 AM34311:08 AM140 p3433411

--- client/src/App.vue
+++ client/src/App.vue
@@ -157,14 +157,13 @@ const searchText = ref("");
             </PopperComponent>
 
             <div
-                class="  rounded-full h-8 self-center mx-2 bg-zinc-700 flex focus-within:bg-gray-600 focus-within:ring-2 ring-blue-500 transition-all">
-                          <input class="px-4 w-52 h-full rounded-full without-ring bg-transparent transition-all " placeholder="Search ..."
-                          :class="searchCollapsed ? 'w-0 px-0 m-0' : ''" v-model="searchText"/>
-                <a href=""
-                                class="fa  text-gray-300 self-center hover:text-blue-500 w-8 h-8 rounded-full flex center hover:bg-gray-500/50 hover-ripple" 
-                    :class="searchCollapsed ? 'fa-search' : 'fa-times'"
-                    @click.prevent="searchCollapsed = !searchCollapsed; searchText = ''"
-                ></a>
+              class="  rounded-full h-8 self-center mx-2 bg-zinc-700 flex focus-within:bg-gray-600 focus-within:ring-2 ring-blue-500 transition-all">
+              <input class="px-4 w-52 h-full rounded-full without-ring bg-transparent transition-all "
+                placeholder="Search ..." :class="searchCollapsed ? 'w-0 px-0 m-0' : ''" v-model="searchText" />
+              <a href=""
+                class="fa  text-gray-300 self-center hover:text-blue-600 w-8 h-8 rounded-full flex center hover:bg-gray-500/50 hover-ripple"
+                :class="searchCollapsed ? 'fa-search' : 'fa-times'"
+                @click.prevent="searchCollapsed = !searchCollapsed; searchText = ''"></a>
             </div>
           </div>
           <div class="nav-center flex items-center transition-all duration-150 flex-nowrap" v-if="window.glb.loggedIn">
