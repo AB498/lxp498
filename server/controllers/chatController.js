f4029c6805593e6448c9a9636ba6f9be6cfbc1f7
AB498 <abcd49800@gmail.com>
2023-05-20 20:15:04 +0000

    gi8:15 PM201406 PM1798:15 PM140 p17948

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -10,11 +10,44 @@ module.exports.listChats = async (req, res) => {
 
 module.exports.createChat = async (req, res) => {
     const user1 = req.user;
-    const user2 = req.body.user2;
-    const newChat = await models.Conversation.create({
+    const user2 = req.body.otherUserId;
+    const user1Id = user1.id;
+    const user2Id = user2;
 
+    const existingChat = = await Conversation.findAll({
+        include: [
+            {
+                model: User,
+                where: {
+                    id: {
+                        [Op.or]: [user1Id, user2Id],
+                    },
+                },
+                through: {
+                    attributes: [], // Exclude join table attributes from the result
+                    where: {
+                        user_id: {
+                            [Op.or]: [user1Id, user2Id],
+                        },
+                    },
+                },
+            },
+        ],
+        group: ['Conversation.id'], // To avoid duplicate conversations
+        having: sequelize.literal(`COUNT(DISTINCT Users.id) = 2`), // Only include conversations with both users
     });
-    await newChat.addUsers([user1, user2]);
-    await newChat.save();
-    return res.send(newChat);
+
+    if (existingChat) {
+        return res.send(existingChat);
+    }
+
+    const conversation = await models.Conversation.create({ name: 'My Conversation' });
+    await conversation.addUsers([user1, user2]);
+
+    const fetchedConversation = await models.Conversation.findOne({
+        where: { id: conversation.id },
+        include: { model: models.User }
+    });
+
+    return res.send(fetchedConversation);
 }
