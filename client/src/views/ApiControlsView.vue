d8997dab4f2168aaf1339fa423bdc7068f17047c
AB498 <abcd49800@gmail.com>
2023-05-20 18:03:23 +0000

    gi6:03 PM201406 PM3946:03 PM140 p394236

--- client/src/views/ApiControlsView.vue
+++ client/src/views/ApiControlsView.vue
@@ -1,5 +1,21 @@
+<script setup>
+
+
+const res = (await fetch('http://lanxplore.xyz/test' + endpoint.url, {
+    method: endpoint.method,
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: body ? body : null
+}));
+
+
+
+</script>
+
+
 <template>
     <div class="w-full h-full bg-red-400">
-        hello
+            {{ res }}
     </div>
 </template>
