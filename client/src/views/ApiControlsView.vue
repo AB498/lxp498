3ef464efd8b55f5657fa8826248199746b95e2c2
AB498 <abcd49800@gmail.com>
2023-05-20 18:08:20 +0000

    gi6:08 PM201406 PM6506:08 PM140 p650206

--- client/src/views/ApiControlsView.vue
+++ client/src/views/ApiControlsView.vue
@@ -5,14 +5,19 @@ const apiEndpoints = (await(await fetch('http://lanxplore.xyz/admin/apis')).json
 
 async function sendRequest(endpoint) {
 
-    const res = (await fetch('http://lanxplore.xyz/test' + endpoint.url, {
+    let res = (await fetch('http://lanxplore.xyz/test' + endpoint.url, {
         method: endpoint.method,
         headers: {
             'Content-Type': 'application/json'
         },
         body: body ? body : null
     }));
+    try {
 
+        res = JSON.stringify(await res.json())
+    } catch (e) {
+        res = await res.text()
+    }
 }
 
 
@@ -20,19 +25,19 @@ async function sendRequest(endpoint) {
 
 
 <template>
-        <div class="w-full h-full bg-red-400">
-            <div v-for="endpoint in apiEndpoints">
-                <div class="flex flex-col bg-fuchsia-950">
-                    <div class="flex w-full  items-center space-x-2 p-2">
+    <div class="w-full h-full bg-red-400">
+        <div v-for="endpoint in apiEndpoints">
+            <div class="flex flex-col bg-fuchsia-950">
+                <div class="flex w-full  items-center space-x-2 p-2">
 
-                            <div class="bg-slate-700 p-1 rounded" v-text="endpoint.method"></div>
-                            <div class="bg-slate-700 p-1 rounded" v-text="'/test' + endpoint.url"></div>
-                        <div x-text="body"></div>
-                        <div class="grow"></div>
-                        <div class="btn" @click="sendRequest(endpoint)">Send</div>
+                    <div class="bg-slate-700 p-1 rounded" v-text="endpoint.method"></div>
+                    <div class="bg-slate-700 p-1 rounded" v-text="'/test' + endpoint.url"></div>
+                    <div x-text="body"></div>
+                    <div class="grow"></div>
+                    <div class="btn" @click="sendRequest(endpoint)">Send</div>
 
-                    </div>
                 </div>
             </div>
+        </div>
     </div>
 </template>
