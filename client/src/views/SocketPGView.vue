d241c70e049f480ca96f038f47dd868dcde5ba88
AB498 <abcd49800@gmail.com>
2023-05-21 05:42:58 +0000

    gi5:42 AM211417 AM8045:42 AM141 p804585

--- client/src/views/SocketPGView.vue
+++ client/src/views/SocketPGView.vue
@@ -1,12 +1,102 @@
 <script setup>
 
+const URL = "http://localhost:3000";
+const socketURL = URL;
+const socketOptions = computed(() => ({
+  extraHeaders: {
+    Authorization: `Bearer ${window.glb?.jwt || ''}`
+  }
+}));
+let syncerSocket = null;
+
+function initializeSocket(){
+    // Create and connect the socket
+    syncerSocket = io(socketURL, socketOptions.value);
+    syncerSocket.connect();
+    
+    syncerSocket.on("connect", () => {
+        console.log("my id: " + syncerSocket.id);
+    
+        syncerSocket.emit("resolveUser", window.glb.user.jwt);
+    });
+    
+    syncerSocket.on("disconnect", () => {
+        console.log("disconnected");
+        window.glb.lxsocket.connected = false;
+    });
+    
+    syncerSocket.on("connect_error", (err) => {
+        console.log("connect_error", err);
+    });
+    
+    syncerSocket.on("connect_timeout", (err) => {
+        console.log("connect_timeout", err);
+    });
+    
+    syncerSocket.on("error", (err) => {
+        console.log("error", err);
+    });
+    
+    syncerSocket.on("reconnect", (attemptNumber) => {
+        console.log("reconnect", attemptNumber);
+    });
+    
+    syncerSocket.on("reconnect_attempt", (attemptNumber) => {
+        console.log("reconnect_attempt", attemptNumber);
+    });
+    
+    syncerSocket.on("reconnecting", (attemptNumber) => {
+        console.log("reconnecting", attemptNumber);
+    });
+    
+    syncerSocket.on("reconnect_error", (err) => {
+        console.log("reconnect_error", err);
+    });
+    
+    syncerSocket.on("reconnect_failed", () => {
+        console.log("reconnect_failed");
+    });
+    
+    syncerSocket.on("ping", () => {
+        console.log("ping");
+    });
+    
+    syncerSocket.on("pong", (latency) => {
+        console.log("pong", latency);
+    });
+    
+    syncerSocket.on("foo", (data) => {
+        console.log("foo", data);
+        window.glb.lxsocket.fooEvents.push(data);
+    });
+    
+    syncerSocket.on("bar", (data) => {
+        console.log("bar", data);
+        window.glb.lxsocket.barEvents.push(data);
+    });
+    
+    syncerSocket.on("onlineUsers", (data) => {
+        console.log("onlineUsers", data);
+        window.glb.lxsocket.onlineUsers = data;
+    });
+    
+    syncerSocket.on("serverSync", (data) => {
+        console.log("serverSync", data);
+        window.glb._serverSynced = data;
+    });
+    
+    syncerSocket.on("serverSynced", (data) => {
+        console.log("serverSynced", data);
+        window.glb._serverSynced = data;
+    });
+}
 
 </script>
 
 <template>
 
 <div class="full">
-
+    
 
 </div>
 
