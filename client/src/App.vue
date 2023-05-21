630ad73c9f06a391055eddeeeb02b8b330ca00bc
AB498 <abcd49800@gmail.com>
2023-05-21 12:54:31 +0000

    gi12:54 PM211417 PM18312:54 PM141 p1833112

--- client/src/App.vue
+++ client/src/App.vue
@@ -34,6 +34,9 @@ rjwatch(window.glb.syncerObj, null, (newVal, oldVal) => {
   window.glb.syncerObj = syncer.syncerObj;
   console.log('syncerObj changed', newVal, oldVal)
 })
+watch(() => window.glb.syncerObj, (newVal, oldVal) => {
+  console.log('watch: syncerObj changed', newVal, oldVal)
+})
 window.glb.syncerObj = syncer.syncerObj;
 window.glb.syncerObj.a = 324
 
