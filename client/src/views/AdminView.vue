2d2ff898ff29567c04cdae619a2d7e44c5868ef3
AB498 <abcd49800@gmail.com>
2023-05-21 05:31:46 +0000

    gi5:31 AM211417 AM5245:31 AM141 p524465

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -44,8 +44,8 @@ async function pollChanges() {
 <template>
   <div class="flex flex-col w-full h-full bg-gray-700">
       <div class="h-10 bg-red-800 w-full">
-        <i class="fas fa-api p-2">APIS</i>
-        <i class="fas fa-api p-2">SocketPG</i>
+          <i class="fas fa-api p-2" @click="router.push('apicontrols')">APIS</i>
+          <i class="fas fa-api p-2" @click="router.push('socketpg')">SocketPG</i>
 
     </div>
 
