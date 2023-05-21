06d1a57cdcab415efc542c8bf87d1e27445b272e
AB498 <abcd49800@gmail.com>
2023-05-21 07:59:33 +0000

    gi7:59 AM211417 AM5277:59 AM141 p527337

--- client/src/views/SocketPGView.vue
+++ client/src/views/SocketPGView.vue
@@ -24,12 +24,6 @@ let syncerObj= createProxy({
     onlineUsers:[],
     conversations:[],
 })
-rjwatch(syncerObj,null, (o,n,p,k,v)=>{ //(oldval, newval, modpath, key, value)
-    console.log(`${p||'root'}->${k} changed from ${JSON.stringify(o)} to ${JSON.stringify(n)}`)
-    if (syncerSocket) {
-        syncerSocket.emit("updateObj", {path:p, value:v});
-    }
-})
 
 initializeSocket()
 function initializeSocket(){
@@ -38,80 +32,27 @@ function initializeSocket(){
     syncerSocket.connect();
     
     syncerSocket.on("connect", () => {
-        console.log("my id: " + syncerSocket.id);
         socketState.connected=true;
-syncerObj.a=43;
+        
+        rjwatch(syncerObj, null, (o, n, p, k, v) => { //(oldval, newval, modpath, key, value)
+        console.log(`${p || 'root'}->${k} changed from ${JSON.stringify(o)} to ${JSON.stringify(n)}`)
+        socket.emit("updateObj", { path: p, value: v });
+        }); //onchange
+
+        socket.on('updateObj', ({ path, value }) => {
+        // console.log(path, value);
+        rjmod(syncerObj, path, value);
+        console.log(syncerObj);
+        }); //onreceive
+        
+        syncerObj.a=43;
     });
     
     syncerSocket.on("disconnect", () => {
         console.log("disconnected");
         socketState.connected=false
     });
-    
-    syncerSocket.on("connect_error", (err) => {
-        console.log("connect_error", err);
-    });
-    
-    syncerSocket.on("connect_timeout", (err) => {
-        console.log("connect_timeout", err);
-    });
-    
-    syncerSocket.on("error", (err) => {
-        console.log("error", err);
-    });
-    
-    syncerSocket.on("reconnect", (attemptNumber) => {
-        console.log("reconnect", attemptNumber);
-    });
-    
-    syncerSocket.on("reconnect_attempt", (attemptNumber) => {
-        console.log("reconnect_attempt", attemptNumber);
-    });
-    
-    syncerSocket.on("reconnecting", (attemptNumber) => {
-        console.log("reconnecting", attemptNumber);
-    });
-    
-    syncerSocket.on("reconnect_error", (err) => {
-        console.log("reconnect_error", err);
-    });
-    
-    syncerSocket.on("reconnect_failed", () => {
-        console.log("reconnect_failed");
-    });
-    
-    syncerSocket.on("ping", () => {
-        console.log("ping");
-    });
-    
-    syncerSocket.on("pong", (latency) => {
-        console.log("pong", latency);
-    });
-    
-    syncerSocket.on("foo", (data) => {
-        console.log("foo", data);
-        window.glb.lxsocket.fooEvents.push(data);
-    });
-    
-    syncerSocket.on("bar", (data) => {
-        console.log("bar", data);
-        window.glb.lxsocket.barEvents.push(data);
-    });
-    
-    syncerSocket.on("onlineUsers", (data) => {
-        console.log("onlineUsers", data);
-        window.glb.lxsocket.onlineUsers = data;
-    });
-    
-    syncerSocket.on("serverSync", (data) => {
-        console.log("serverSync", data);
-        window.glb._serverSynced = data;
-    });
-    
-    syncerSocket.on("serverSynced", (data) => {
-        console.log("serverSynced", data);
-        window.glb._serverSynced = data;
-    });
+  
 }
 
 </script>
