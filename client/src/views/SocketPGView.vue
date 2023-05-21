b01d84018dcc55e1e33727152bc1902e3c14819a
AB498 <abcd49800@gmail.com>
2023-05-21 06:25:24 +0000

    gi6:25 AM211417 AM6156:25 AM141 p615246

--- client/src/views/SocketPGView.vue
+++ client/src/views/SocketPGView.vue
@@ -3,7 +3,8 @@ import { ref, watch, onUnmounted, onMounted, nextTick, computed } from 'vue'
 import Toggle from '@vueform/toggle'
 import { io } from "socket.io-client";
 import { reactive } from 'vue';
-import { createProxy, rjwatch, rjmod } from '@/composables/ReactiveJSON'
+import obj from '@/composables/ReactiveJSON'
+const  { createProxy, rjwatch, rjmod } = obj
 
 const URL = "http://localhost:3000";
 const socketURL = URL;
