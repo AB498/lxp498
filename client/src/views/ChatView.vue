beb9a516c04a7b1ba475cb68e690195ec4423080
AB498 <abcd49800@gmail.com>
2023-05-21 11:17:33 +0000

    gi11:17 AM211417 AM32211:17 AM141 p3223311

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -7,6 +7,7 @@ watch(() => window.glb.lxsocket.onlineUsers, (newVal, oldVal) => {
   // console.log('onlineUsers changed', newVal, oldVal)
 })
 
+window.glb.chats = ref(await window.glb.safeAuthedReq('/api/getAllUsers'))
 
 // async function openConversation() {
 //   console.log('openConversation')
@@ -30,9 +31,9 @@ watch(() => window.glb.lxsocket.onlineUsers, (newVal, oldVal) => {
   <div class="w-full h-full  bg-slate-800 ">
     <div class="flex w-full h-full">
       <div class="basis-1/4 h-full bg-cyan-900">
-        <div class="text-2xl p-2" v-loading-bar="{ loading: !window.glb._serverSynced.onlineUsers }" v-ripple>Online</div>
-          <div class="" v-if="window.glb._serverSynced.conversations">
-            <div v-for="(user, index) in window.glb._serverSynced.onlineUsers" :key="index">
+          <div class="text-2xl p-2" v-loading-bar="{ loading: !window.glb.chats }" v-ripple>Online</div>
+            <div class="" v-if="window.glb.chats">
+              <div v-for="(user, index) in window.glb.chats" :key="index">
               <div class="btn" @click="" v-ripple>
                 {{ user.email }}
               </div>
