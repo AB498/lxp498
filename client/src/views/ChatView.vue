2a47f346e1b260a2fa6c57e9966806eafc9524c2
AB498 <abcd49800@gmail.com>
2023-05-21 16:34:33 +0000

    gi4:34 PM211417 PM2624:34 PM141 p262334

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -39,7 +39,7 @@ window.glb.syncerObj.openChat = {}
           <div class="text-2xl p-2" v-loading-bar="{ loading: !window.glb.chats }" v-ripple>Online</div>
             <div class="" v-if="window.glb.chats">
               <div v-for="(user, index) in window.glb.chats" :key="index">
-                                    <div class="btn hover-ripple-fast" @click="window.glb.syncerObj.openChat.email = user.email; router.push('/chat/' + user.email)">
+                                      <div class="btn hover-ripple-fast" @click="window.glb.syncerObj.openChat.email = user.email; router.push('/chat/' + user.id)">
                   {{ user.email }}
               </div>
             </div>
