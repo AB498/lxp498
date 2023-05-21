b9186a9fe1e68a2db91032e00be69825f682e79a
AB498 <abcd49800@gmail.com>
2023-05-21 07:51:07 +0000

    gi7:51 AM211417 AM7997:51 AM141 p79977

--- client/src/composables/ReactiveJSON.js
+++ client/src/composables/ReactiveJSON.js
@@ -149,9 +149,18 @@ function fastObjCopy(obj) {
 
 
 
+let obj = { createProxy, rjwatch, rjmod, rjget }
+if (typeof window != 'undefined') {
+    window.rjexport = obj
+    window.createProxy = createProxy
+    window.rjwatch = rjwatch
+    window.rjmod = rjmod
+    window.rjget = rjget
+}
+try {
 
     console.log('module')
-    module.exports.createProxy = createProxy;
-    module.exports.rjwatch = rjwatch;
-    module.exports.rjmod = rjmod;
-module.exports.rjget = rjget;
+    export default obj
+} catch (e) {
+    console.log(e)
+}
