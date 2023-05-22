ef4d4192ccb48c6c095f10a9c39ac67624feb6ab
AB498 <abcd49800@gmail.com>
2023-05-22 10:26:49 +0000

    gi10:26 AM221421 AM44810:26 AM142 p4484910

--- client/src/App.vue
+++ client/src/App.vue
@@ -35,7 +35,7 @@ syncer.init()
 
 rjwatch(syncer.syncerObj, null, (o, n, p, k, v) => {
   if (p == "/openChat/messages") {
-    console.log(...v[0]);
+    console.log(JSON.stringify(v[0]));
   }
   window.glb.syncerObj = JSON.parse(JSON.stringify({ ...syncer.syncerObj }));
   window.glb.syncerObj = syncer.syncerObj;
