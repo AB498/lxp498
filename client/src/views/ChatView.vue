25c3d605757824106eb58573c65242e6d7cb3a22
AB498 <abcd49800@gmail.com>
2023-05-22 12:59:28 +0000

    gi12:59 PM221421 PM35512:59 PM142 p3552812

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -36,7 +36,7 @@ async function openConversation(user) {
                 <img :src="user.pfpUrl" class="w-8 h-8 rounded-full inline-block mr-2">
                 <div class="flex flex-col">
                   <div class="name">{{ user.firstName + ' ' + user.lastName }}</div>
-                    <div class="username">{{ user.username || 'nousername' }}</div>
+                      <div class="username text-sm">{{ user.username || '@nousername' }}</div>
                 </div>
 
 
