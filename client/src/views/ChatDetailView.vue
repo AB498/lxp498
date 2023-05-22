a45f874b0c28a4c33208b89efc199d62a7bda328
AB498 <abcd49800@gmail.com>
2023-05-22 13:21:29 +0000

    gi1:21 PM221421 PM1301:21 PM142 p130291

--- client/src/views/ChatDetailView.vue
+++ client/src/views/ChatDetailView.vue
@@ -77,7 +77,8 @@ function fastObjCopy(obj) {
                 <div class="text-2xl p-2 ">{{ window.glb.syncerObj.openChat.otherUser?.firstName || 'Username' }}</div>
                 <div class="h-full bg-teal-900 flex flex-col  overflow-auto" id="chat-messages"
                     v-if="window.glb.syncerObj.openChat.messages">
-                    <div v-for="(message, index) in window.glb.syncerObj.openChat.messages" :key="message.id">
+                        <div v-for="(message, index) in window.glb.syncerObj.openChat.messages" :key="message.id"
+                        class="divide-y">
                         <UserMessage :message="message"
                             :user="window.glb.syncerObj.openChat.participants.find(u => u.id == message.UserId)"
                             index="message.id" />
