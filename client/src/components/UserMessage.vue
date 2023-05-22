9b5e8599a33363ceff4c0d6bdf1f9b84efb46d29
AB498 <abcd49800@gmail.com>
2023-05-22 10:02:09 +0000

    gi10:02 AM221421 AM28510:02 AM142 p285910

--- client/src/components/UserMessage.vue
+++ client/src/components/UserMessage.vue
@@ -34,7 +34,7 @@ async function deleteMessage() {
                     {{ mes.UserId.username || 'No Name' }} {{ window.glb.getFormattedTime(new Date(mes.createdAt)) }} {{ mes.id
                     }}
                 </div>
-                            <div class="text-md font-thin">
+                                <div class="text-md ">
                     {{ mes.text }}
                 </div>
 
