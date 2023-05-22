404a617d05edd9ff3dcb44f2b6bcf8bd9796d053
AB498 <abcd49800@gmail.com>
2023-05-22 09:37:02 +0000

    gi9:37 AM221421 AM6109:37 AM142 p61029

--- client/src/views/ChatDetailView.vue
+++ client/src/views/ChatDetailView.vue
@@ -35,7 +35,7 @@ async function openConversation(id) {
     let res = await window.glb.safeAuthedReq('/api/openConversation', { id: id })
     if (res) {
         window.glb.syncerObj.openChat.participants = res.Users;
-        window.glb.syncerObj.openChat.otherUser = res.Users.find(u => u.id != window.glb.syncerObj.user.id);
+        window.glb.syncerObj.openChat.otherUser = res.Users.find(u => u.id != window.glb.user.id);
     } else {
         window.glb.addNotf('error', 'Error creating chat')
     }
