eb1d4a74caeb66f307bc07afa2ab46e0cd33ca47
AB498 <abcd49800@gmail.com>
2023-05-22 09:42:31 +0000

    gi9:42 AM221421 AM0949:42 AM142 p094319

--- client/src/components/UserMessage.vue
+++ client/src/components/UserMessage.vue
@@ -26,7 +26,7 @@ async function deleteMessage() {
 </script>
 
 <template>
-                                                <div class="w-full p-3 border-b-[1] border-gray-200 flex center space-x-2" v-if="mes && mes.UserId" @mouseover="showActions = true"
+                                                    <div class="w-full p-3 border-b-[1px] border-gray-200 flex center space-x-2" v-if="mes && mes.UserId" @mouseover="showActions = true"
             @mouseout="showActions = false">
                                     <img class="w-6 h-6" :src="userInc.pfpUrl || '/logo.svg'" />
             <div class="flex flex-col w-full h-full">
