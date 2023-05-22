2c3bd4aeb79f020bd313fddbf24103c280692e6f
AB498 <abcd49800@gmail.com>
2023-05-22 14:45:08 +0000

    gi2:45 PM221421 PM4782:45 PM142 p47882

--- client/src/components/UserMessage.vue
+++ client/src/components/UserMessage.vue
@@ -45,7 +45,7 @@ async function deleteMessage() {
                     @click="editMessage"> </button>
                 <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-trash"
                     @click="deleteMessage"> </button>
-                    <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fa-solid fa-globe"
+                        <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-globe"
                     @click="deleteMessage"> </button>
                     <img :src="'/langauge.png'" class="w-8 h-8 rounded-full inline-block mr-2">
 
