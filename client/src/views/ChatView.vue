8dc069c98164f1f943b831321fc1750173219cad
AB498 <abcd49800@gmail.com>
2023-05-22 12:57:01 +0000

    gi12:57 PM221421 PM69512:57 PM142 p695112

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -28,7 +28,7 @@ async function openConversation(user) {
 <template>
   <div class="w-full h-full  bg-slate-800 ">
     <div class="flex w-full h-full">
-      <div class="basis-1/4 h-full bg-cyan-900">
+        <div class="basis-1/4 h-full bg-cyan-950">
         <div class="text-2xl p-2" v-loading-bar="{ loading: !window.glb.chats }" v-ripple>Online</div>
         <div class="" v-if="window.glb.chats">
           <div v-for="(user, index) in window.glb.chats" :key="index">
