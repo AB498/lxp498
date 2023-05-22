83f04498978439953d002a9d95eb804e383777d5
AB498 <abcd49800@gmail.com>
2023-05-22 10:04:07 +0000

    gi10:04 AM221421 AM59610:04 AM142 p596710

--- client/src/composables/glb.js
+++ client/src/composables/glb.js
@@ -134,7 +134,6 @@ store.getTime = () => {
   let ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0' + minutes : minutes;
   let strTime = hours + ':' + minutes + ':' + secmilli + ' ' + ampm;
   return strTime;
 }
@@ -147,7 +146,6 @@ store.getFormattedTime = (date) => {
   let ampm = hours >= 12 ? 'pm' : 'am'
   hours = hours % 12
   hours = hours ? hours : 12 // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0' + minutes : minutes
   let strTime = hours + ':' + minutes + ' ' + ampm
 
   return strTime
