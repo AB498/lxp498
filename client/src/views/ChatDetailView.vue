8757fc3b954c96a9da0b42a985823e7bf776464c
AB498 <abcd49800@gmail.com>
2023-05-22 18:12:33 +0000

    gi6:12 PM221421 PM4916:12 PM142 p491336

--- client/src/views/ChatDetailView.vue
+++ client/src/views/ChatDetailView.vue
@@ -65,7 +65,7 @@ initialLoad.value = false
 </script>
 
 <template>
-    <div class="w-full h-full  bg-slate-800 overflow-auto"></div>
+        <div class="w-full h-full  bg-slate-800 overflow-auto">
         <div class="w-full h-full  bg-slate-800 overflow-auto"
             v-loading-bar="{ loading: !window.glb.syncerObj.openChat?.conversationId }">
             <div class="flex flex-col w-full h-full overflow-auto" v-if="window.glb.syncerObj.openChat?.conversationId">
