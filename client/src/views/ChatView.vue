aaf1b28c1aa0131692fe52cfab271daca12765dd
AB498 <abcd49800@gmail.com>
2023-05-22 12:57:45 +0000

    gi12:57 PM221421 PM75912:57 PM142 p7594512

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -29,11 +29,14 @@ async function openConversation(user) {
   <div class="w-full h-full  bg-slate-800 ">
     <div class="flex w-full h-full">
         <div class="basis-1/4 h-full bg-cyan-950">
-        <div class="text-2xl p-2" v-loading-bar="{ loading: !window.glb.chats }" v-ripple>Online</div>
-        <div class="" v-if="window.glb.chats">
-          <div v-for="(user, index) in window.glb.chats" :key="index">
-                    <div class="bg-slate-600 p-2 m-1 rounded hover-ripple-fast" @click="openConversation(user)">
-              {{ user.firstName + ' '+ user.lastName }}
+          <div class="text-2xl p-2" v-loading-bar="{ loading: !window.glb.chats }" v-ripple>Online</div>
+          <div class="" v-if="window.glb.chats">
+            <div v-for="(user, index) in window.glb.chats" :key="index">
+              <div class="bg-slate-600 p-2 m-1 rounded hover-ripple-fast" @click="openConversation(user)">
+                <img :src="user.avatar" class="w-8 h-8 rounded-full inline-block mr-2">
+                <div class="name">{{ user.firstName + ' ' + user.lastName }}</div>
+
+              
             </div>
           </div>
         </div>
