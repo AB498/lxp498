7780ee68c02f175a4b55bd83888f45bf8ff8b299
AB498 <abcd49800@gmail.com>
2023-05-22 14:00:22 +0000

    gi2:00 PM221421 PM9752:00 PM142 p975222

--- client/src/components/UserMessage.vue
+++ client/src/components/UserMessage.vue
@@ -26,27 +26,27 @@ async function deleteMessage() {
 </script>
 
 <template>
-            <div class="w-full p-3 flex center space-x-2 hover:bg-slate-600 relative" v-if="mes && mes.UserId"
-                @mouseover="showActions = true" @mouseout="showActions = false">
-                <img class="w-8 h-8 self-start rounded-full" :src="userInc.pfpUrl || '/logo.svg'" />
-                <div class="flex flex-col w-full h-full">
-                    <div class="text-xs text-gray-300">
-                        {{ (user.firstName + ' ' + user.lastName) }} {{ window.glb.getFormattedTime(new Date(mes.createdAt)) }}
-                    </div>
-                    <div class="text-md whitespace-pre-wrap">
-                        {{ mes.text }}
-                    </div>
-
-
+        <div class="w-full p-3 flex center space-x-2 hover:bg-slate-600 relative" v-if="mes && mes.UserId"
+            @mouseover="showActions = true" @mouseout="showActions = false">
+            <img class="w-8 h-8 self-start rounded-full" :src="userInc.pfpUrl || '/logo.svg'" />
+            <div class="flex flex-col w-full h-full">
+                <div class="text-xs text-gray-300">
+                    {{ (user.firstName + ' ' + user.lastName) }} {{ window.glb.getFormattedTime(new Date(mes.createdAt)) }}
                 </div>
-                <!-- cross button -->
-                <div class="flex absolute top-2 right-2 flex-row-reverse" :class="showActions ? 'visible' : 'invisible'">
-                    <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-pen"
-                        @click="editMessage"> </button>
-                    <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-trash"
-                        @click="deleteMessage"> </button>
-                            <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fa-solid fa-langauge"
-                            @click="deleteMessage"> </button>
+                <div class="text-md whitespace-pre-wrap">
+                    {{ mes.text }}
+                </div>
+
+
+            </div>
+            <!-- cross button -->
+            <div class="flex absolute top-2 right-2 flex-row-reverse" :class="showActions ? 'visible' : 'invisible'">
+                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-pen"
+                    @click="editMessage"> </button>
+                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-trash"
+                    @click="deleteMessage"> </button>
+                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fa-solid fa-langauge"
+                    @click="deleteMessage"> </button>
             </div>
         </div>
 </template>
