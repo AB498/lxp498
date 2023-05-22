6aea14b4c3da1311b624cf9d2af4c05bba6a761c
AB498 <abcd49800@gmail.com>
2023-05-22 14:25:44 +0000

    gi2:25 PM221421 PM4212:25 PM142 p421442

--- client/src/components/UserMessage.vue
+++ client/src/components/UserMessage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import  feather from 'feather-icons')
+import feather from 'feather-icons'
 import { defineProps, ref } from 'vue'
 
 const props = defineProps({
@@ -27,31 +27,31 @@ async function deleteMessage() {
 </script>
 
 <template>
-        <div class="w-full p-3 flex center space-x-2 hover:bg-slate-600 relative" v-if="mes && mes.UserId"
-            @mouseover="showActions = true" @mouseout="showActions = false">
-            <img class="w-8 h-8 self-start rounded-full" :src="userInc.pfpUrl || '/logo.svg'" />
-            <div class="flex flex-col w-full h-full">
-                <div class="text-xs text-gray-300">
-                    {{ (user.firstName + ' ' + user.lastName) }} {{ window.glb.getFormattedTime(new Date(mes.createdAt)) }}
-                </div>
-                <div class="text-md whitespace-pre-wrap">
-                    {{ mes.text }}
-                </div>
+            <div class="w-full p-3 flex center space-x-2 hover:bg-slate-600 relative" v-if="mes && mes.UserId"
+                @mouseover="showActions = true" @mouseout="showActions = false">
+                <img class="w-8 h-8 self-start rounded-full" :src="userInc.pfpUrl || '/logo.svg'" />
+                <div class="flex flex-col w-full h-full">
+                    <div class="text-xs text-gray-300">
+                        {{ (user.firstName + ' ' + user.lastName) }} {{ window.glb.getFormattedTime(new Date(mes.createdAt)) }}
+                    </div>
+                    <div class="text-md whitespace-pre-wrap">
+                        {{ mes.text }}
+                    </div>
 
 
-            </div>
-            <!-- cross button -->
-            <div class="flex absolute top-2 right-2 flex-row-reverse" :class="showActions ? 'visible' : 'invisible'">
-                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-pen"
-                    @click="editMessage"> </button>
-                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-trash"
-                    @click="deleteMessage"> </button>
-                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fa-solid fa-langauge"
+                </div>
+                <!-- cross button -->
+                <div class="flex absolute top-2 right-2 flex-row-reverse" :class="showActions ? 'visible' : 'invisible'">
+                    <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-pen"
+                        @click="editMessage"> </button>
+                    <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-trash"
                         @click="deleteMessage"> </button>
-{{ feather.icons.circle.toSvg({ class: 'foo bar' })
-     }}
+                    <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fa-solid fa-langauge"
+                            @click="deleteMessage"> </button>
+    {{ feather.icons.circle.toSvg({ class: 'foo bar' })
+    }}
 
-            </div>
+                </div>
         </div>
 </template>
 
