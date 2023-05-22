d26d3727d67e97312e8d41ad916e44ebecf130bf
AB498 <abcd49800@gmail.com>
2023-05-22 15:03:04 +0000

    gi3:03 PM221421 PM0513:03 PM142 p05143

--- client/src/components/UserMessage.vue
+++ client/src/components/UserMessage.vue
@@ -42,16 +42,14 @@ async function deleteMessage() {
             <!-- cross button -->
             <div class="flex absolute top-2 right-2 flex-row-reverse" :class="showActions ? 'visible' : 'invisible'">
                 <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-pen"
-                        @click="editMessage"> <q-tooltip> Edit</q-tooltip></button>
-                    <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-trash"
-                        @click="deleteMessage"> <q-tooltip> Delete</q-tooltip></button>
-                            <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross"
-                                        @click="deleteMessage">
-                                             <span class="material-symbols-outlined p-3 text-lg">
-                        translate
-                        </span>         <q-tooltip> Translate Mode</q-tooltip>
-                        </button>
-            
+                    @click="editMessage"> <q-tooltip> Edit</q-tooltip></button>
+                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross fas fa-trash"
+                    @click="deleteMessage"> <q-tooltip> Delete</q-tooltip></button>
+                <button class="bg-blue-600 shadow p-2 m-1 rounded hover-ripple-fast flex center-cross" @click="deleteMessage">
+                    <span class="material-symbols-outlined text-lg">translate</span>
+                    <q-tooltip> Translate Mode</q-tooltip>
+                </button>
+
 
             </div>
         </div>
