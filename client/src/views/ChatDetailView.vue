20a8710bf84345a582aaadb6309fcd54db790d91
AB498 <abcd49800@gmail.com>
2023-05-22 13:27:14 +0000

    gi1:27 PM221421 PM8991:27 PM142 p899141

--- client/src/views/ChatDetailView.vue
+++ client/src/views/ChatDetailView.vue
@@ -81,7 +81,7 @@ function fastObjCopy(obj) {
                             <UserMessage :message="message"
                                 :user="window.glb.syncerObj.openChat.participants.find(u => u.id == message.UserId)"
                                 index="message.id" />
-                            <q-separator   spaced="12px"/>
+                                <div class="h-2 p-4 bg-gray-200 w-full"></div>
                             </div>
                 </div>
                 <div class="flex justify-center">
