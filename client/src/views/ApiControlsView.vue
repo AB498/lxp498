4727e5a185f2a156a50ff6692d6f3f249131d80c
AB498 <abcd49800@gmail.com>
2023-05-22 16:12:29 +0000

    gi4:12 PM221421 PM5024:12 PM142 p502294

--- client/src/views/ApiControlsView.vue
+++ client/src/views/ApiControlsView.vue
@@ -62,8 +62,7 @@ async function sendRequest(endpoint) {
                             :class="endpoint.method == 'GET' ? 'bg-orange-400' : 'bg-blue-500'"></div>
                         <div class="bg-slate-700 p-1 rounded" v-text="'/test' + endpoint.url"></div>
                             <div v-for="param in endpoint.params" v-if="endpoint.params">
-                                            <q-input v-model="param.value" outlined dense class="bg-slate-700 text-white" :label="param.key"
-                                    :placeholder="param.key" dark></q-input>
+                                            <q-input v-model="param.value" outlined dense class="bg-slate-700 text-white" :label="param.key"     dark></q-input>
                         </div>
                         <div class="grow"></div>
                         <i class="material-icons text-2xl text-lime-500 hover:bg-lime-500 hover:text-slate-900 cursor-pointer rounded-full w-8 h-8"
