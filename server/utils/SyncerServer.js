bc6d7dfcf81f9fd9c7a4d14fd28daf4260257de9
AB498 <abcd49800@gmail.com>
2023-05-23 09:04:56 +0000

    gi9:04 AM231432 AM1509:04 AM143 p150569

--- server/utils/SyncerServer.js
+++ server/utils/SyncerServer.js
@@ -84,7 +84,7 @@ makeServer = (server) => {
                         let inf = vidInfo.data.items[0].snippet
                         vid = (await models.Video.create({ ytId: v, title: inf.title, description: inf.description, thumbnailUrl: inf.thumbnails.default.url, info: inf })).dataValues;
                     }
-                    vid.inf;
+                    console.log(typeof (vid.inf);
                     syncerObj.openYTVideo.videoInfo = vid;
                 }
             } catch (e) {
