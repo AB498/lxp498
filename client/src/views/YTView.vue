d5c17e1b3a3250bd8bf763c00fe30c91fe17eb5e
AB498 <abcd49800@gmail.com>
2023-05-23 18:07:07 +0000

    gi6:07 PM231432 PM9376:07 PM143 p93776

--- client/src/views/YTView.vue
+++ client/src/views/YTView.vue
@@ -7,7 +7,6 @@ import { ref, watch, onUnmounted, onMounted, nextTick } from 'vue'
 import SuggestedVideos from "@/components/SuggestedVideos.vue";
 import LoadingSpin from "../components/LoadingSpin.vue";
 import bs from "binary-search";
-import Clusterize from "clusterize.js";
 
 // => 2
 
@@ -22,7 +21,6 @@ const player = ref(null)
 const ytPlayerReady = ref(false)
 const playerState = ref(null)
 const words = ref([])
-const wordsRe = ref([])
 const playerReadyCallbacks = ref([() => {
   ytPlayerReady.value = true;
 }])
@@ -121,13 +119,11 @@ watch(() => window.glb.syncerObj?.openYTVideo?.subtitlesStatus, async (newVal, o
   if (newVal == 1) {
     window.glb.addNotf('Fetching subtitles...')
     words.value = await window.glb.safeAuthedReq('/api/getYTSubtitles/' + route.params.slug)
-    wordsRe.value = words.value.slice(m - 5, m + 5)
-
     // window.syncer.destroy();
   }
 })
 let lastactword = null;
-const playerMainLoop = setInterval(async () => {
+const playerMainLoop = setInterval(() => {
 
   if (ytPlayerReady.value) {
     let currentTime = player.value.getCurrentTime();
@@ -151,7 +147,7 @@ const playerMainLoop = setInterval(async () => {
           l = m + 1;
         }
       }
-      wordsRe.value = words.value.slice(m - 50, m + 50)
+
       let actword = words.value[m];
       actword.active = true;
       document.getElementById('subWordsHolderId').scrollTop = (actword.el.offsetTop - document.getElementById('subWordsHolderId').offsetTop);
@@ -165,39 +161,35 @@ const playerMainLoop = setInterval(async () => {
   }
 }, 500)
 
-onMounted(() => {
-  // JavaScript
-
-})
 </script>
 <template>
-      <div class="flex items-center justify-center h-full">
-        <div class="flex bg-gray-900 text-white flex-wrap h-full w-full">
-          <div class="video-and-suggestions flex flex-col sm:basis-4/6 overflow-auto h-full">
+    <div class="flex items-center justify-center h-full">
+      <div class="flex bg-gray-900 text-white flex-wrap h-full w-full">
+        <div class="video-and-suggestions flex flex-col sm:basis-4/6 overflow-auto h-full">
 
-            <div class=" sm:h-96 h-80 w-full flex flex-col pointer-none sticky top-0 items-center z-10 shrink-0">
-              <!-- video -->
-              <iframe id="ytPlayerElement" class="w-full h-full border-4 bg-zinc-900" :src="'https://www.youtube.com/embed/'
-                + '' + '?enablejsapi=1&mute=1&autoplay=1&controls=0&showinfo=0&disablekb=1'" :class="borderColor"></iframe>
+          <div class=" sm:h-96 h-80 w-full flex flex-col pointer-none sticky top-0 items-center z-10 shrink-0">
+            <!-- video -->
+            <iframe id="ytPlayerElement" class="w-full h-full border-4 bg-zinc-900" :src="'https://www.youtube.com/embed/'
+              + '' + '?enablejsapi=1&mute=1&autoplay=1&controls=0&showinfo=0&disablekb=1'" :class="borderColor"></iframe>
 
-              <div class="yt-video-player-slider-holder w-full -translate-y-2  relative transition shrink-0">
-                <div class=" yt-video-player-slider-bg bg-gray-700 w-full bg-gray-800/50 h-2 absolute">
-                </div>
-                <div class=" yt-video-player-slider-bg bg-gray-700 w-full opacity-0 h-2 absolute z-10" x-ref="totalBar"
-                  x-init="$watch('draggingSlider', draggingSlider => console.log(draggingSlider))">
-                </div>
-                <div class=" yt-video-player-slider bg-gray-700 h-2 absolute " :style="{ width: videoProgress + '%' }">
-                  <!--mousedown -->
-                  <div
-                    class=" yt-video-player-slider-cursor rounded-full h-2 bg-red-600 w-4 hover:scale-150 scale-125 right-0 absolute translate-x-1/2">
-                  </div>
+            <div class="yt-video-player-slider-holder w-full -translate-y-2  relative transition shrink-0">
+              <div class=" yt-video-player-slider-bg bg-gray-700 w-full bg-gray-800/50 h-2 absolute">
+              </div>
+              <div class=" yt-video-player-slider-bg bg-gray-700 w-full opacity-0 h-2 absolute z-10" x-ref="totalBar"
+                x-init="$watch('draggingSlider', draggingSlider => console.log(draggingSlider))">
+              </div>
+              <div class=" yt-video-player-slider bg-gray-700 h-2 absolute " :style="{ width: videoProgress + '%' }">
+                <!--mousedown -->
+                <div
+                  class=" yt-video-player-slider-cursor rounded-full h-2 bg-red-600 w-4 hover:scale-150 scale-125 right-0 absolute translate-x-1/2">
                 </div>
               </div>
-              <div class="w-full h-20 subtitles  bg-zinc-900 text-gray-300  shrink-0           ">
-                <div class="flex items-center justify-center flex-wrap overflow-auto w-full h-full scroll-smooth "
-                  id="subWordsHolderId"
-                  v-if="window.glb.syncerObj?.openYTVideo?.subtitlesStatus == 1 && glb.isIterable(words) && words.length > 0">
-                  <div v-for="(word, index) in wordsRe" :key="index" :ref="(el) => { word.el = el }" class="p-1 pb-0 h-10">
+            </div>
+            <div class="w-full h-20 subtitles  bg-zinc-900 text-gray-300  shrink-0           ">
+              <div class="flex items-center justify-center flex-wrap overflow-auto w-full h-full scroll-smooth "
+                id="subWordsHolderId"
+                v-if="window.glb.syncerObj?.openYTVideo?.subtitlesStatus == 1 && glb.isIterable(words) && words.length > 0">
+                <div v-for="(word, index) in words" :key="index" :ref="(el) => { word.el = el }" class="p-1 pb-0 h-10">
                   <PowerWord :word-inc="word">
                   </PowerWord>
                 </div>
@@ -277,15 +269,9 @@ onMounted(() => {
           </div>
         </div>
 
-        <div class="comments flex-col bg-gray-600 text-white sm:basis-1/6 grow overflow-auto h-full sm:flex ">
-                <div id="scrollArea" class="clusterize-scroll">
-              <div id="contentArea" class="clusterize-content">
-            <div class="clusterize-no-data">Loading data…</div>
-          </div>
-        </div>
-      </div>
       </div>
       <!-- <script src="/src/js/youtube-iframe-setup.js"></script>
                                                                                                                                                                                                                                                                                                                                                                                                                                                                 <script src="/src/parts/yt.js"></script> -->
+      <div class="comments flex-col bg-gray-900 text-white sm:basis-1/6 grow overflow-auto h-full sm:flex hidden"></div>
   </div>
 </template>
