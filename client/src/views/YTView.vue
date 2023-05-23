d343f2ade9f283241336abfb0d344c0408c96f0d
AB498 <abcd49800@gmail.com>
2023-05-23 08:45:15 +0000

    gi8:45 AM231432 AM6968:45 AM143 p696158

--- client/src/views/YTView.vue
+++ client/src/views/YTView.vue
@@ -121,69 +121,68 @@ initialLoad.value = false;
           <iframe id="ytPlayerElement" class="w-full h-full border-4 bg-zinc-900" :src="'https://www.youtube.com/embed/'
             + '' + '?enablejsapi=1&mute=1&autoplay=1&controls=0&showinfo=0&disablekb=1'" :class="borderColor"></iframe>
 
-            <div class="yt-video-player-slider-holder w-full -translate-y-2  relative transition shrink-0">
-              <div class=" yt-video-player-slider-bg bg-gray-700 w-full bg-gray-800/50 h-2 absolute">
-              </div>
-              <div class=" yt-video-player-slider-bg bg-gray-700 w-full opacity-0 h-2 absolute z-10" x-ref="totalBar"
-                x-init="$watch('draggingSlider', draggingSlider => console.log(draggingSlider))">
-              </div>
-              <div class=" yt-video-player-slider bg-gray-700 h-2 absolute ">
-                <!--mousedown -->
-                <div
-                  class=" yt-video-player-slider-cursor rounded-full h-2 bg-red-600 w-4 hover:scale-150 scale-125 right-0 absolute translate-x-1/2">
-                </div>
+          <div class="yt-video-player-slider-holder w-full -translate-y-2  relative transition shrink-0">
+            <div class=" yt-video-player-slider-bg bg-gray-700 w-full bg-gray-800/50 h-2 absolute">
+            </div>
+            <div class=" yt-video-player-slider-bg bg-gray-700 w-full opacity-0 h-2 absolute z-10" x-ref="totalBar"
+              x-init="$watch('draggingSlider', draggingSlider => console.log(draggingSlider))">
+            </div>
+            <div class=" yt-video-player-slider bg-gray-700 h-2 absolute ">
+              <!--mousedown -->
+              <div
+                class=" yt-video-player-slider-cursor rounded-full h-2 bg-red-600 w-4 hover:scale-150 scale-125 right-0 absolute translate-x-1/2">
               </div>
             </div>
-            <div class="w-full h-20 subtitles  bg-zinc-900 text-gray-300  shrink-0           ">
-              <div class="flex items-center justify-center flex-wrap overflow-auto w-full h-full scroll-smooth "
-                id="subWordsHolderId"
-                v-if="window.glb.videoInfo.subtitlesAvailable == 1 && glb.isIterable(words) && words.length > 0">
-                <div v-for="(word, index) in words" :key="index" :ref="(el) => { word.el = el }" class="p-1 pb-0 h-10">
-                  <PowerWord :word-inc="word">
-                  </PowerWord>
+          </div>
+          <div class="w-full h-20 subtitles  bg-zinc-900 text-gray-300  shrink-0           ">
+            <div class="flex items-center justify-center flex-wrap overflow-auto w-full h-full scroll-smooth "
+              id="subWordsHolderId"
+              v-if="window.glb.videoInfo.subtitlesAvailable == 1 && glb.isIterable(words) && words.length > 0">
+              <div v-for="(word, index) in words" :key="index" :ref="(el) => { word.el = el }" class="p-1 pb-0 h-10">
+                <PowerWord :word-inc="word">
+                </PowerWord>
+              </div>
+            </div>
+            <div v-else-if="window.glb.videoInfo.subtitlesAvailable == -1"
+              class=" w-full h-full flex items-center justify-center">
+              <div class="btn" @click="requestSubGen">Generate Subtitles
+                <i class="fa fa-bolt"></i>
+              </div>
+            </div>
+            <div v-else-if="window.glb.videoInfo.subtitlesAvailable == 0"
+              class=" w-full h-full flex items-center justify-center">
+              <div class=" flex items-center px-2 space-x-2">
+                <div>
+                  Subtitles Generation in Progress
                 </div>
-              </div>
-              <div v-else-if="window.glb.videoInfo.subtitlesAvailable == -1"
-                class=" w-full h-full flex items-center justify-center">
-                <div class="btn" @click="requestSubGen">Generate Subtitles
-                  <i class="fa fa-bolt"></i>
-                </div>
-              </div>
-              <div v-else-if="window.glb.videoInfo.subtitlesAvailable == 0"
-                class=" w-full h-full flex items-center justify-center">
-                <div class=" flex items-center px-2 space-x-2">
-                  <div>
-                    Subtitles Generation in Progress
-                  </div>
-                  <LoadingSpin />
-                </div>
-              </div>
-              <div v-else-if="window.glb.videoInfo.subtitlesAvailable == -2"
-                class=" w-full h-full flex items-center justify-center">
-                <PopperComponent>
-                  <template #tohover>
-                    <div class="flex space-x-2 items-center justify-center ">
-
-                      <div class="btn" @click="requestSubGen">Generate Subtitles
-                        <i class="fa fa-bolt"></i>
-                      </div>
-                      <i class="fa fa-warning text-yellow-400"></i>
-                    </div>
-                  </template>
-                  <template #popup>
-                    <div class="">
-                      Error Occured Previosly Generating Subtitles
-                    </div>
-                  </template>
-                </PopperComponent>
-              </div>
-
-              <div v-else class="flex center w-full h-full">
                 <LoadingSpin />
-
               </div>
             </div>
-            <div class="">{{ window.glb.syncerObj.openYTVideo.videoInfo.title }}</div>
+            <div v-else-if="window.glb.videoInfo.subtitlesAvailable == -2"
+              class=" w-full h-full flex items-center justify-center">
+              <PopperComponent>
+                <template #tohover>
+                  <div class="flex space-x-2 items-center justify-center ">
+
+                    <div class="btn" @click="requestSubGen">Generate Subtitles
+                      <i class="fa fa-bolt"></i>
+                    </div>
+                    <i class="fa fa-warning text-yellow-400"></i>
+                  </div>
+                </template>
+                <template #popup>
+                  <div class="">
+                    Error Occured Previosly Generating Subtitles
+                  </div>
+                </template>
+              </PopperComponent>
+            </div>
+
+            <div v-else class="flex center w-full h-full">
+              <LoadingSpin />
+
+            </div>
+          </div>
 
           <div class=" controls h-10 bg-indigo-900 flex items-stretch justify-center px-2 shrink-0 self-stretch">
             <i class="fa flex items-center px-2 hover:bg-blue-600 hover:outline fa-play"></i>
@@ -205,6 +204,8 @@ initialLoad.value = false;
             <i class="flex items-center px-2 hover:bg-blue-600 hover:outline fa fa-gear"></i>
           </div>
         </div>
+          <div class="">{{ window.glb.syncerObj.openYTVideo.videoInfo.title }}</div>
+
         <div class="suggested-videos bg-gray-900 text-white sm:flex-wrap sm:flex ">
           <SuggestedVideos />
         </div>
