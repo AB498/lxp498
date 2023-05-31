0c31ac42ac49fa4ff0c9b119897de0e5ff2b5e0f
AB498 <abcd49800@gmail.com>
2023-05-31 10:09:51 +0000

    gi10:09 AM311513 AM43310:09 AM151 p4335110

--- client/src/components/VideoCardVertical.vue
+++ client/src/components/VideoCardVertical.vue
@@ -26,8 +26,8 @@ import pic from "@/assets/logo.svg";
                 <div>Play</div>
             </div>
         </div>
+        <p class="w-full text-ellipsis overflow-auto text-xs line-clamp-3 break-words" v-text="video.title || 'No Title'"></p>
         <div class="w-full p-4 break-words overflow-auto">
-            <p class="w-full text-ellipsis overflow-auto text-xs line-clamp-3 break-words" v-text="video.title || 'No Title'"></p>
             <p class="w-full text-ellipsis overflow-auto text-xs themed-text-secondary line-clamp-2 break-words" v-text="video.description || 'No Description'">
             </p>
         </div>
