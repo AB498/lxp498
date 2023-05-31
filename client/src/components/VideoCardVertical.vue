41331b6221a1958c355d4b08e976027136c8a5d3
AB498 <abcd49800@gmail.com>
2023-05-31 10:03:50 +0000

    gi10:03 AM311513 AM15010:03 AM151 p1505010

--- client/src/components/VideoCardVertical.vue
+++ client/src/components/VideoCardVertical.vue
@@ -27,9 +27,9 @@ import pic from "@/assets/logo.svg";
             </div>
         </div>
         <div class="w-full p-4 break-words">
-            <p class="w-full text-xs line-clamp-3 break-words" v-text="video.title || 'No Title'"></p>
-            <p class="w-full text-xs themed-text-secondary line-clamp-2 break-words" v-text="video.description || 'No Description'">
-            </p>
+            <div class="w-full text-xs line-clamp-3 break-words" v-text="video.title || 'No Title'"></div>
+            <div class="w-full text-xs themed-text-secondary line-clamp-2 break-words" v-text="video.description || 'No Description'">
+            </div>
         </div>
     </div>
 </template>
