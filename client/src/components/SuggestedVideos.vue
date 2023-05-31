ec48c4d069cfebd46ea2504dbd3ff0ec79330b5d
AB498 <abcd49800@gmail.com>
2023-05-31 10:16:32 +0000

    gi10:16 AM311513 AM52710:16 AM151 p5273210

--- client/src/components/SuggestedVideos.vue
+++ client/src/components/SuggestedVideos.vue
@@ -14,7 +14,7 @@ onMounted(async () => {
 
 <template>
   <div class="flex flex-col sm:flex-row sm:flex-wrap ">
-      <div v-for="(video, index) in videos" :key="index" class=" basis-1/4 p-4 ">
+      <div v-for="(video, index) in videos" :key="index" class=" basis-1/4 shrink-0 grow-0 p-4 ">
       <VideoCardVertical :video="{
         thumbnailUrl: video.snippet.thumbnails.medium.url,
         title: video.snippet.title, description: video.snippet.channelTitle,
